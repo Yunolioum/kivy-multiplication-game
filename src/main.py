5178d425bd7931b8d147b6f4c111315c6ee9c18d
Robin Persson <robin.persson@gmail.com>
2013-10-14 23:04:23 +0200

    Numpad can now be used for input.
    Both keyboard and numpad uses the same input mechanisms.

--- src/main.py
+++ src/main.py
@@ -8,7 +8,21 @@ from kivy.uix.button import Button
 from kivy.properties import NumericProperty, ObjectProperty, BooleanProperty, StringProperty
 
 class Numpad(Widget):
-    pass
+    def __init__(self, **kwargs):
+        super(Numpad, self).__init__(**kwargs)
+        self.register_event_type('on_new_number')
+        self.register_event_type('on_erase_number')
+    
+    def numbutton_pressed(self, instance, value):
+        self.dispatch('on_new_number', int(value))
+        
+    def erase_pressed(self, instance):
+        self.dispatch('on_erase_number')
+        
+    def on_new_number(self, number):
+        pass
+    def on_erase_number(self):
+        pass
 
 class NumButton(Button):
     pass
@@ -37,35 +51,42 @@ class SuccessDependantGraphics(Widget):
         self.position_y_multiplier = -1
     
 class NumericTextInput(TextInput):
-    ALLOWED_KEYS = [
-        'backspace',
-        'numpad0',
-        'numpad1',
-        'numpad2',
-        'numpad3',
-        'numpad4',
-        'numpad5',
-        'numpad6',
-        'numpad7',
-        'numpad8',
-        'numpad9',
-        '0','1','2','3','4',
-        '5','6','7','8','9',
-    ]
+    NUMPAD_KEYS = {
+        'numpad0': '0',
+        'numpad1': '1',
+        'numpad2': '2',
+        'numpad3': '3',
+        'numpad4': '4',
+        'numpad5': '5',
+        'numpad6': '6',
+        'numpad7': '7',
+        'numpad8': '8',
+        'numpad9': '9',
+    }
     
     def __init__(self, **kwargs):
         super(NumericTextInput, self).__init__(**kwargs)
         self.register_event_type('on_enter')
+        self.register_event_type('on_new_number')
+        self.register_event_type('on_erase_number')
     
     def _keyboard_on_key_down(self, window, keycode, text, modifiers):
         __unused_key, key_str = keycode
-        if key_str in self.ALLOWED_KEYS:
-            super(NumericTextInput, self)._keyboard_on_key_down(window, keycode, text, modifiers)
+        if key_str in self.NUMPAD_KEYS.values():
+            self.dispatch('on_new_number', key_str)
+        elif self.NUMPAD_KEYS.has_key(key_str):
+            self.dispatch('on_new_number', self.NUMPAD_KEYS.get(key_str))
+        elif key_str == 'backspace':
+            self.dispatch('on_erase_number')
         elif key_str == 'enter':
             self.dispatch('on_enter')
             pass
             
     def on_enter(self):
+        pass
+    def on_new_number(self, number):
+        pass
+    def on_erase_number(self):
         pass
 
 class GameBoard(Widget):
@@ -74,7 +95,7 @@ class GameBoard(Widget):
     
     correct_answer = 0
     
-    current_guess = StringProperty('')
+    _current_guess = StringProperty('')
     
     input = ObjectProperty(None)
     new_game_button = ObjectProperty(None)
@@ -83,16 +104,34 @@ class GameBoard(Widget):
     
     success_dependant_graphics = ObjectProperty(None)
     
+    numpad = ObjectProperty(None)
+    
     def __init__(self, **kwargs):
         super(GameBoard, self).__init__(**kwargs)
-        self.input.bind(text=self.on_text)
-        self.input.bind(on_enter=self.on_enter)
         self.success_dependant_graphics.new_game_button.bind(
                 on_press=self.on_new_game_button_clicked)
+        self.numpad.bind(on_new_number=self.on_appended_number)
+        self.numpad.bind(on_erase_number=self.on_erase_number)
+        self.input.bind(on_new_number=self.on_appended_number)
+        self.input.bind(on_erase_number=self.on_erase_number)
+        self.input.bind(on_enter=self.on_enter)
+        
+    @property
+    def current_guess(self):
+        return self._current_guess
     
-    def on_text(self, instance, text):
-        self.current_guess = text
-        self.on_guess()
+    @current_guess.setter
+    def current_guess(self, new_text):
+        if len(new_text) > 0:
+            int(new_text) # Make sure we only input integers
+        if not self._current_guess == str(self.correct_answer):
+            self._current_guess = new_text
+            self.on_guess()
+    def on_appended_number(self, instance, number):
+        self.current_guess += str(number)
+    def on_erase_number(self, instance):
+        if len(self.current_guess) > 0:
+            self.current_guess = self.current_guess[:(len(self.current_guess) - 1)]
     
     def on_guess(self):
         if self.current_guess == str(self.correct_answer):
@@ -108,7 +147,7 @@ class GameBoard(Widget):
         
     def on_new_game_button_clicked(self, instance):
         if not self.input_is_correct:
-            raise RuntimeError("Tried to start a new game")
+            raise RuntimeError("Tried to start a new game with an incorrect answer")
         else:
             self.new_game()
         
@@ -122,8 +161,6 @@ class GameBoard(Widget):
     def new_game(self):
         self.input_is_correct = False
         
-        self.input.text = ''
-        
         self.operand1 = randint(1,12)
         self.operand2 = randint(1,12)
         
@@ -131,6 +168,8 @@ class GameBoard(Widget):
         
         self.success_dependant_graphics.hide()
         self.success_dependant_graphics.on_new_game()
+        
+        self.current_guess = ''
 
         self.input.focus = True
         
